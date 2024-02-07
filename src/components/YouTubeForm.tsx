c8f1973f43120d9a4c37b73b3d2e055326974630
vinaybeelagi <vinaykumar.b@neoito.com>
2024-02-07 12:39:27 +0530

    hookform to control forms from devtool

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,21 +1,24 @@
 import { useForm } from 'react-hook-form';
-
+import {DevTool} from '@hookform/devtools'
 
 export const YouTubeForm = () => {
-  
+  const form = useForm();
+  const {register,control} = form;
+    
   return (
     <div>
         <form>
-            <label htmlFor= "username">UserName</label>
-            <input type="text" name="username" id="username" />
+            <label htmlFor= "username">Username</label>
+            <input type="text"  id="username"{...register("username")} />
             <label htmlFor= "email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" id="email" {...register("email")} />
             <label htmlFor= "channel">channel</label>
-            <input type="text" name="channel" id="channel" />
+            <input type="text"  id="channel" {...register("channel")}/>
 
             <button type="submit">Submit</button>
 
         </form>
+        <DevTool control={control} />
     </div>
   )
 }
