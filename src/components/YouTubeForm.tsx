5e585eb5323b4a51bd4d3b68094040bd0ac50a29
vinaybeelagi <vinaykumar.b@neoito.com>
2024-02-07 13:23:49 +0530

    Form submission:onSubmit,handlesubmit,formvalues are added

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,13 +1,25 @@
 import { useForm } from 'react-hook-form';
 import {DevTool} from '@hookform/devtools'
 
+  
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+}
+
 export const YouTubeForm = () => {
-  const form = useForm();
-  const {register,control} = form;
-    
+  const form = useForm <FormValues>();
+  const {register,control,handleSubmit} = form;
+  
+  const onSubmit = (data:FormValues) => {
+    console.log("form Submitted",data);
+  }
+
+
   return (
-    <div>
-        <form>
+    <div> 
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor= "username">Username</label>
             <input type="text"  id="username"{...register("username")} />
             <label htmlFor= "email">Email</label>
